7a867c6c1f6becd243b7c2bdb4fe0e49055812de
Filipino9507 <56320187+Filipino9507@users.noreply.github.com>
2020-10-26 19:55:02 +0100

    AI game options

--- src/app/play-ai/play-ai.component.ts
+++ src/app/play-ai/play-ai.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
 interface GameSettings {
-  difficulty: 1 | 2 | 3 | 4 | 5
-  playerWhite: boolean
-  secondsToThink: number
-  secondsIncrement: number
+  difficulty?: 0 | 1 | 2 | 3 | 4;
+  playerColor: number;
+  secondsToThink: number;
+  secondsIncrement: number;
 }
 
 @Component({
@@ -14,16 +14,26 @@ interface GameSettings {
 })
 export class PlayAIComponent implements OnInit {
 
+  public readonly difficultyOptions = ['Very easy', 'Easy', 'Medium', 'Hard', 'Very hard'];
+  public readonly playerColorOptions = ['White', 'Black'];
+  public readonly secondsToThinkOptions = [60, 180, 300, 600, 1800];
+  public readonly secondsIncrementOptions = [0, 2, 5, 10, 20];
+
   private gameOn: boolean;
-  private gameSettings: GameSettings;
+  public gameSettings: GameSettings;
   
   public constructor() { }
 
   public ngOnInit(): void {
-
+    this.gameSettings = {
+      difficulty: 3,
+      playerColor: 0,
+      secondsToThink: 300,
+      secondsIncrement: 0
+    };
   }
 
   public startGame(): void {
-
+    console.log(this.gameSettings)
   }
 }
